7393ed9de7d5297ac19806921dfab431281c7fd4
iimagii <mag_1@ukr.net>
2023-04-16 13:02:17 +0300

    fix for deploy 1.01

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -7,12 +7,12 @@ import {
 import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID
+  apiKey: 'AIzaSyAPJB-BOjmbymi6ZNN96_VbWuhcWvP9V2s',
+  authDomain: 'crm-test-50a2c.firebaseapp.com',
+  projectId: 'crm-test-50a2c',
+  storageBucket: 'crm-test-50a2c.appspot.com',
+  messagingSenderId: '377634802889',
+  appId: '1:377634802889:web:75fb9346b74b6b263f1118'
 }
 
 const app = initializeApp(firebaseConfig)
